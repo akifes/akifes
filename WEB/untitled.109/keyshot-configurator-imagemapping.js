cfdff4437e4a3e7f7bf80f5ec8ece1414dfc7695
akifes <sefika.fe@outlook.com>
2024-12-15 14:32:12 +0100

    akifes
    git add --all
    
    git commit -m "Initial commit"
    
    git push -u origin main

---
+++ WEB/untitled.109/keyshot-configurator-imagemapping.js
@@ -0,0 +1,30 @@
+imagemappingdata =
+[
+    {
+        "image": "untitled.109.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "1015568748+11"
+        ]
+    },
+    {
+        "image": "untitled.109.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "1015568748+14"
+        ]
+    },
+    {
+        "image": "untitled.109.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "1015568748+37"
+        ]
+    }
+];
---
+++ WEB/untitled.109/keyshot-configurator-modeldata.js
@@ -0,0 +1,44 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "cube%20material.png",
+                    "multimaterials": [
+                        "1015568748+11"
+                    ],
+                    "name": "Cube Material"
+                },
+                {
+                    "icon": "cube%20material%20%231.png",
+                    "multimaterials": [
+                        "1015568748+14"
+                    ],
+                    "name": "Cube Material #1"
+                },
+                {
+                    "icon": "cube%20material%20%232.png",
+                    "multimaterials": [
+                        "1015568748+37"
+                    ],
+                    "name": "Cube Material #2"
+                }
+            ],
+            "name": "Cube Material"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Cube Material"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
